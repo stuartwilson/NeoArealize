0da478cdac24e282362b70a90fc2c1fe54730d55
Seb James <seb.james@sheffield.ac.uk>
2018-08-20 14:32:43 +0100

    Whitespace removal

--- sim/ermentrout.cpp
+++ sim/ermentrout.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main (int argc, char **argv){
-  
+
     if (argc < 2) {
         cerr << "\nUsage: ./build/sim/process w0\n\n";
         cerr << "Be sure to run from the base source directory.\n";
@@ -18,14 +18,14 @@ int main (int argc, char **argv){
     vector<double> fix(3, 0.0);
     vector<double> eye(3, 0.0);
     vector<double> rot(3, 0.0);
-    
+
     double rhoInit = 1.4;
     string worldName(argv[1]);
     string winTitle = worldName + ": n";
     displays.push_back (morph::Gdisplay (500, 500, 100, 0, winTitle.c_str(), rhoInit, 0.0, 0.0));
     displays.back().resetDisplay (fix, eye, rot);
     displays.back().redrawDisplay();
-    
+
     rhoInit = 1.4;
     winTitle = worldName + ": c";
     displays.push_back (morph::Gdisplay (500, 500, 100, 0, winTitle.c_str(), rhoInit, 0.0, 0.0));
@@ -39,11 +39,11 @@ int main (int argc, char **argv){
     } catch (const exception& e) {
         cerr << "Exception initialising RD_2D_Karb object: " << e.what() << endl;
     }
-    
+
     M.Dn = stod(argv[3]);
     M.chi = M.Dn;
     M.Dc = 0.3*M.Dn;
-    
+
     // Start the loop
     bool doing = true;
     while (doing) {
@@ -54,7 +54,7 @@ int main (int argc, char **argv){
             cerr << "Caught exception calling M.step(): " << e.what() << endl;
             doing = false;
         }
-        
+
         displays[0].resetDisplay (fix, eye, rot);
         try {
             M.plot (displays);
@@ -63,6 +63,6 @@ int main (int argc, char **argv){
             doing = false;
         }
     }
-    
+
     return 0;
 };
